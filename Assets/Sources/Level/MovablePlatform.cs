38d71cc93ac3c3a3741ee86dd5d295f506e375d1
Vladisvell <vladisvell@gmail.com>
2024-03-31 20:57:57 +0500

    Switched to OnValidate method usage

--- Assets/Sources/Level/MovablePlatform.cs
+++ Assets/Sources/Level/MovablePlatform.cs
@@ -24,22 +24,21 @@ public class MovablePlatform : MonoBehaviour
     void FixedUpdate()
     {
         if (!_shouldMove)
-            return;
-        var rb = InvalidateRigidBody();
-        rb.velocity = _velocity;
+            return; 
+        _rigidbody.velocity = _velocity;
         bool snapBodies = false;
         //if we moved outside the box; just reverse velocity params and snap them inside the bounding box
-        if (rb.position.x < _leftBoundX || rb.position.x > _rightBoundX)
+        if (_rigidbody.position.x < _leftBoundX || _rigidbody.position.x > _rightBoundX)
         {
             _velocity.x = -_velocity.x;
-            rb.position += _velocity * Time.fixedDeltaTime;
+            _rigidbody.position += _velocity * Time.fixedDeltaTime;
             snapBodies = true;
         }
             
-        if (rb.position.y < _downBoundY || rb.position.y > _upBoundY)
+        if (_rigidbody.position.y < _downBoundY || _rigidbody.position.y > _upBoundY)
         {
             _velocity.y = -_velocity.y;
-            rb.position += _velocity * Time.fixedDeltaTime;
+            _rigidbody.position += _velocity * Time.fixedDeltaTime;
             snapBodies = true;
         }
         
@@ -73,13 +72,12 @@ public class MovablePlatform : MonoBehaviour
         UnSubscribeObjectOnCollisionMovement(body);
     }
 
-    private Rigidbody2D InvalidateRigidBody()
+    private void OnValidate()
     {
         if (_rigidbody == null)
         {
             _rigidbody = GetComponent<Rigidbody2D>();
-            return _rigidbody;
         }
-        return _rigidbody;
     }
+
 }
