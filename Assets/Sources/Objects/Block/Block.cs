08e5adbaee3b936e366ed4dca0cc27eb308f71f4
Vladisvell <vladisvell@gmail.com>
2024-04-17 12:47:05 +0500

    Added pushblocks prefabs

--- Assets/Sources/Objects/Block/Block.cs
+++ Assets/Sources/Objects/Block/Block.cs
@@ -8,37 +8,55 @@ public class Block : MonoBehaviour
     [SerializeField]
     private float maxSpeed = 1.0f;
 
-    [SerializeField] 
+    [SerializeField]
+    private float speedDecay = 0.01f;
+
+    [SerializeField]
     private Rigidbody2D _rb;
+
+    [SerializeField]
+    private RigidBodySensor _sensor;
+
+    private Vector2 _platformSpeed = Vector2.zero;
 
     private void FixedUpdate()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
-        if (hit.collider != null)
+        if (hit.collider != null && _rb.bodyType != RigidbodyType2D.Static && hit.collider.attachedRigidbody != null)
         {
+            if (_platformSpeed != hit.collider.attachedRigidbody.velocity)
+            {
+                _platformSpeed = hit.collider.attachedRigidbody.velocity;
+                _rb.position += _platformSpeed * _platformSpeed * Time.fixedDeltaTime * Time.fixedDeltaTime;
+            }
+
+            //Find maximum plausible speed
             Vector2 clampVel = _rb.velocity;
-            var attachedRbMagnitude = hit.collider.attachedRigidbody.velocity.magnitude;
-            clampVel.x = Mathf.Clamp(clampVel.x, -maxSpeed - attachedRbMagnitude, maxSpeed + attachedRbMagnitude);
+            Vector2 platformVelocity = hit.collider.attachedRigidbody.velocity; 
+            Vector2 selfSpeed = clampVel - platformVelocity; //Speed component owned only by our object
+            var attachedRbMagnitudeX = MathF.Abs(hit.collider.attachedRigidbody.velocity.x);
+            //Clamp it
+            selfSpeed.x = Mathf.Clamp(selfSpeed.x, -maxSpeed, maxSpeed);
+            //Decay it
+            selfSpeed.x -= selfSpeed.x * speedDecay;
+            //Apply it
+            _rb.velocity = selfSpeed + platformVelocity;
+        }
+        else
+        {
+            _platformSpeed = Vector2.zero;
+            Vector2 clampVel = _rb.velocity;
+            clampVel.x = Mathf.Clamp(clampVel.x, -maxSpeed, maxSpeed);
+            clampVel.x -= clampVel.x * speedDecay;
             _rb.velocity = clampVel;
         }
     }
 
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    //Slows down block on collision exit, so it won't slide
-    //    _rb.velocity *= 0.1f; 
-    //}
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        //Slows down block on collision, so it won't slide
-        _rb.velocity *= 0.1f; 
-    }
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
-        if(_rb == null)
+        if (_rb == null)
             _rb = GetComponent<Rigidbody2D>();
     }
 #endif
