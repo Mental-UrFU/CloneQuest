cf23b815ea9e7031ed7453888c891edf605befae
Vladisvell <vladisvell@gmail.com>
2024-06-06 13:58:49 +0500

    Added correct save handling

--- Assets/Scenes/TestLevelLoad/LevelMenu.cs
+++ Assets/Scenes/TestLevelLoad/LevelMenu.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
-public class TESTLevelMenu : MonoBehaviour
+public class LevelMenu : MonoBehaviour
 {
     [SerializeField] RectTransform _container;
     [SerializeField] Button _buttonTemplate;
@@ -19,19 +20,24 @@ public class TESTLevelMenu : MonoBehaviour
     private void CreateButtons()
     {
         _buttons = _levelIds.Select(id => Instantiate(_buttonTemplate, _container)).ToArray();
+        Dictionary<string, int> levelStars = new Dictionary<string, int>();
+        List<LevelData> _levelData = new();
+        LevelRepository.Get(_levelIds, SetLevelStars);
+        void SetLevelStars(List<LevelData> levelData)
+        {
+            _levelData = levelData;
+        }
+
         for (var i = 0; i < _buttons.Length; i++)
         {
-            if (!PersistentLevelData.LevelStars.ContainsKey(i))
-            {
-                PersistentLevelData.LevelStars.Add(i, -1);
-            }
             var button = _buttons[i];
             var levelIndex = i;
             button.GetComponentInChildren<TMP_Text>().text = $"{levelIndex + 1}";
-            button.GetComponentInChildren<StarcounterSetter>().DisplayStarsCount(PersistentLevelData.LevelStars[levelIndex]);
-            if (PersistentLevelData.LevelStars[levelIndex] == -1 && i != 0 && !_unlockAllLevels)
+            button.GetComponentInChildren<StarcounterSetter>().DisplayStarsCount(_levelData[levelIndex].Stars);
+            if (_levelData[levelIndex].Passed == false && i != 0 && !_unlockAllLevels)
             {
-                button.GetComponentInChildren<StarcounterSetter>().DisplayLock();
+                if (!_levelData[levelIndex - 1].Passed)
+                    button.GetComponentInChildren<StarcounterSetter>().DisplayLock();
             }
             else
             {
@@ -42,8 +48,7 @@ public class TESTLevelMenu : MonoBehaviour
 
     private void LoadLevel(int index)
     {
-        var context = new LevelContext(index, Array.AsReadOnly(_levelIds), SceneManager.GetActiveScene().name);
-        PersistentLevelData.CurrentLevel = index;
+        var context = new LevelContext(index, Array.AsReadOnly(_levelIds), SceneManager.GetActiveScene().name);        
         LevelManager.Load(context);
     }
 
