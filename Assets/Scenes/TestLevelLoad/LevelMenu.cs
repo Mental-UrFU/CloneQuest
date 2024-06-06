45d3975d9432443a363791c8ed674cd13254f31f
Vladisvell <vladisvell@gmail.com>
2024-06-06 18:02:14 +0500

    Save system!

--- Assets/Scenes/TestLevelLoad/LevelMenu.cs
+++ Assets/Scenes/TestLevelLoad/LevelMenu.cs
@@ -14,54 +14,25 @@ public class LevelMenu : MonoBehaviour
     [SerializeField] bool _unlockAllLevels;
     [HideInInspector][SerializeField] string[] _levelIds;
 
-    
-    private Button[] _buttons;
-
     private void CreateButtons()
     {
-        _buttons = _levelIds.Select(id => Instantiate(_buttonTemplate, _container)).ToArray();
-        Dictionary<string, int> levelStars = new Dictionary<string, int>();
-        List<LevelData> _levelData = new();
-        LevelRepository.Get(_levelIds, SetLevelStars);
-        void SetLevelStars(List<LevelData> levelData)
+        var pass = true;
+        LevelRepository.Get(_levelIds, Create);
+        void Create(List<LevelData> dataList) { for (var i = 0; i < dataList.Count; i++) { CreateButton(i, dataList[i]); } }
+        void CreateButton(int index, LevelData levelData)
         {
-            _levelData = levelData;
-        }
-
-        for (var i = 0; i < _buttons.Length; i++)
-        {
-            var button = _buttons[i];
-            var levelIndex = i;
-            button.GetComponentInChildren<TMP_Text>().text = $"{levelIndex + 1}";
-            button.GetComponentInChildren<StarcounterSetter>().DisplayStarsCount(_levelData[levelIndex].Stars);
-            if (_levelData[levelIndex].Passed == false && i != 0 && !_unlockAllLevels)
-            {
-                if (!_levelData[levelIndex - 1].Passed)
-                    button.GetComponentInChildren<StarcounterSetter>().DisplayLock();
-            }
-            else
-            {
-                button.onClick.AddListener(() => LoadLevel(levelIndex));
-            }            
+            var button = Instantiate(_buttonTemplate, _container);
+            button.GetComponentInChildren<TMP_Text>().text = $"{index + 1}";
+            button.GetComponentInChildren<StarcounterSetter>().DisplayStarsCount(levelData.Stars);
+            if (!pass) { button.GetComponentInChildren<StarcounterSetter>().DisplayLock(); }
+            else { button.onClick.AddListener(() => LevelManager.Load(CreateLevelContext(index))); }
+            if (!levelData.Passed) { pass = false; }
         }
     }
 
-    private void LoadLevel(int index)
-    {
-        var context = new LevelContext(index, Array.AsReadOnly(_levelIds), SceneManager.GetActiveScene().name);        
-        LevelManager.Load(context);
-    }
+    private LevelContext CreateLevelContext(int index) => new(index, Array.AsReadOnly(_levelIds), SceneManager.GetActiveScene().name);
 
-    private void LoadLevelMetadata()
-    {
-
-    }
-
-    private void Awake()
-    {
-        CreateButtons();
-        LoadLevelMetadata();
-    }
+    private void Awake() => CreateButtons();
 
 #if UNITY_EDITOR
     [SerializeField] SceneAsset[] _levelAssets;
@@ -69,7 +40,7 @@ public class LevelMenu : MonoBehaviour
     {
         _levelIds = _levelAssets
             .Select(scene => scene.name)
-            .ToArray();        
+            .ToArray();
     }
 #endif
 }
